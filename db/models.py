c637577fa1b73c15fa100c2d6f2c26a1be5ca2a3
ku113p <ku113p@gmail.com>
2020-11-05 15:39:10 +0300

    step

--- db/models.py
+++ db/models.py
@@ -1,14 +1,10 @@
-from sqlalchemy import Integer, Column, String, Text, Enum, UniqueConstraint, Table, ForeignKey
+import datetime
+
+from sqlalchemy import Integer, Column, String, Text, Enum, Table, ForeignKey, JSON, DateTime
 from sqlalchemy.orm import relationship
 
 from db.db import alchemy_db
 from db.enums import PhraseType
-
-
-class Scenario(alchemy_db.Model):
-    id = Column(Integer, primary_key=True)
-    name = Column(String(80), unique=True, nullable=False)
-    parts = relationship('Part', back_populates='scenario')
 
 
 PartPhrases = Table(
@@ -17,6 +13,25 @@ PartPhrases = Table(
     Column('part_id', Integer, ForeignKey('part.id')),
     Column('phrase_id', Integer, ForeignKey('phrase.id'))
 )
+
+
+ScenarioHistory = Table(
+    'scenario_history',
+    alchemy_db.metadata,
+    Column('scenario_id', Integer, ForeignKey('scenario.id')),
+    Column('history_id', Integer, ForeignKey('history.id'))
+)
+
+
+class Scenario(alchemy_db.Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), unique=True, nullable=False)
+    parts = relationship('Part', back_populates='scenario')
+
+    histories = relationship(
+        'History',
+        secondary=ScenarioHistory,
+        back_populates='scenarios')
 
 
 class Part(alchemy_db.Model):
@@ -28,6 +43,7 @@ class Part(alchemy_db.Model):
     phrases = relationship(
         'Phrase',
         secondary=PartPhrases,
+        lazy='dynamic',
         back_populates='parts')
 
 
@@ -40,3 +56,24 @@ class Phrase(alchemy_db.Model):
         'Part',
         secondary=PartPhrases,
         back_populates='phrases')
+
+    def as_dict(self):
+        return {
+            'title': self.title,
+            'file_id': self.file_id
+        }
+
+
+class History(alchemy_db.Model):
+    id = Column(Integer, primary_key=True)
+    scenario_id = Column(Integer)
+    questioner_id = Column(Integer)
+    answerer_id = Column(Integer)
+    data = Column(JSON)
+    start_date = Column(DateTime, default=datetime.datetime.utcnow)
+    end_date = Column(DateTime)
+
+    scenarios = relationship(
+        'Scenario',
+        secondary=ScenarioHistory,
+        back_populates='histories')
